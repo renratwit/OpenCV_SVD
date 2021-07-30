8a349d5de79d5a0e0d84c0effcbca35af5bb09f5
renratwit <raymond.ren9@gmail.com>
2021-07-30 17:18:37 -0400

    GUI

--- face_detect_demo.py
+++ face_detect_demo.py
@@ -2,6 +2,10 @@ import cv2
 import math
 from face import Face
 import numpy as np
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+from PIL import Image, ImageTk
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('HS.xml')
@@ -32,97 +36,150 @@ head_cascade = cv2.CascadeClassifier('face_front.xml')
 ######
 
 
-# Read the input image
-img = cv2.imread('./pictures/single_mask.png')
-# Convert into grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def detect(n):
+    img = ''
 
+    if n == 1:
+        # Read the input image
+        path = filedialog.askopenfilename()
+        img = cv2.imread(path)
 
-def draw_lines_again(frame, face_list):
-    for f in face_list:
-        for f2 in face_list:
-            if f.x == f2.x and f.y == f2.y:
-                continue
-            center_1 = ((f.x + int(f.w / 2), (f.y + int(f.h / 2))))
-            center_2 = ((f2.x + int(f2.w / 2), (f2.y + int(f2.h / 2))))
-            dist = math.sqrt((f.x - f2.x)**2 + (f.y - f2.y)**2)
-            inches = (dist / f2.w) * 16
-            print("DIST: ", str(inches))
-            if inches <= 50:
-                cv2.line(frame, center_1, center_2, (0, 0, 255), 2)
-    return 0
+    if n == 0:
+        cam = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cam.read()
 
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            HS = face_cascade.detectMultiScale(gray, 1.1, 4)
+            for (x, y, w, h) in HS:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 100, 0), 2)
+            cv2.imshow("Input", frame)
+            k = cv2.waitKey(1)
+            if k % 256 == 27:
+                print("Closing...")
+                break
+            elif k % 256 == 32:
+                img_name = "OpenCV_Input.png"
+                cv2.imwrite("./pictures/openCV_Input.png", frame)
+                img = cv2.imread("./pictures/openCV_Input.png")
+                break
 
-def compareColorAverage(top_matrix, bottom_matrix):
-    top_average = np.mean(top_matrix)
-    bottom_average = np.mean(bottom_matrix)
-    print("TOP AVG: ", str(top_average))
-    print("BOTTOM AVG: ", str(bottom_average))
-    color_ratio = abs(top_average - bottom_average)
-    return color_ratio
+        cam.release()
+        # cv2.namedWindow("Input")
 
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect faces + shoulders
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-face_list = []
-for (x, y, w, h) in faces:
-    cFace = Face(x, y, w, h)
-    face_list.append(cFace)
+    def draw_lines_again(frame, face_list):
+        for f in face_list:
+            for f2 in face_list:
+                if f.x == f2.x and f.y == f2.y:
+                    continue
+                center_1 = ((f.x + int(f.w / 2), (f.y + int(f.h / 2))))
+                center_2 = ((f2.x + int(f2.w / 2), (f2.y + int(f2.h / 2))))
+                dist = math.sqrt((f.x - f2.x)**2 + (f.y - f2.y)**2)
+                inches = (dist / f2.w) * 16
+                print("DIST: ", str(inches))
+                if inches <= 50:
+                    cv2.line(frame, center_1, center_2, (0, 0, 255), 2)
+        return 0
 
-# create a list of just faces
-heads = []
-for h in head_cascade.detectMultiScale(img, 1.1, 4):
-    heads.append(h)
+    def compareColorAverage(top_matrix, bottom_matrix):
+        top_average = np.mean(top_matrix)
+        bottom_average = np.mean(bottom_matrix)
+        print("TOP AVG: ", str(top_average))
+        print("BOTTOM AVG: ", str(bottom_average))
+        color_ratio = abs(top_average - bottom_average)
+        return color_ratio
 
-for face in face_list:
-    print(face.x)
+    # Detect faces + shoulders
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    face_list = []
+    for (x, y, w, h) in faces:
+        cFace = Face(x, y, w, h)
+        face_list.append(cFace)
 
-    # Isolate head+shoulder into matrix
-    face_mat = gray[face.y: face.y_end, face.x: face.x_end]
-    # cv2.imshow(str(face.x), face_mat)
-    # Try to find a face in head+shoulder matrix
-    head = head_cascade.detectMultiScale(face_mat, 1.1, 4)
+    # create a list of just faces
+    heads = []
+    for h in head_cascade.detectMultiScale(img, 1.1, 4):
+        heads.append(h)
 
-    if len(head) > 0:  # If no head is detected, then mask is found
-        face.isMask = True
-        face.box_color = (0, 0, 255)
+    for face in face_list:
+        print(face.x)
 
-    for(x, y, w, h) in head:
-        # Must add (face.x and face.y) to rectangle to account for dimension of entire image
-        cv2.rectangle(img, (x+face.x, y+face.y),
-                      (x+face.x+w, y+face.y+h), (0, 0, 255), 1)
-        matrix = gray[y+face.y:face.y+y+h, face.x+x:face.x+x+w]
+        # Isolate head+shoulder into matrix
+        face_mat = gray[face.y: face.y_end, face.x: face.x_end]
+        # cv2.imshow(str(face.x), face_mat)
 
-        top_matrix = matrix[0:y, :]
-        bottom_matrix = matrix[y:y+h, :]
+        # Try to find a face in head+shoulder matrix
+        head = head_cascade.detectMultiScale(face_mat, 1.1, 4)
 
-        cv2.imshow(str(y), top_matrix)
-        cv2.imshow(str(x), bottom_matrix)
-
-        color_ratio = compareColorAverage(top_matrix, bottom_matrix)
-        if color_ratio <= 20:
+        if len(head) > 0:  # If no head is detected, then mask is found
             face.isMask = True
-        else:
-            face.isMask = False
+            face.box_color = (0, 0, 255)
 
-# Draw things
-for f in face_list:
-    print("MASK: ", f.isMask)
-    box_color = (0, 255, 0)
-    if f.isMask:
-        box_color = (0, 0, 255)
-    cv2.rectangle(img, (f.x, f.y), (f.x_end, f.y_end), box_color, 2)
-    # Display distance from camera
-    # cv2.putText(img,
-    #             str(f.est_dist),
-    #             (f.x, f.y),
-    #             f.font, 1,
-    #             (0, 255, 0),
-    #             2,
-    #             cv2.LINE_4)
+        for(x, y, w, h) in head:
+            # Must add (face.x and face.y) to rectangle to account for dimension of entire image
+            cv2.rectangle(img, (x+face.x, y+face.y),
+                          (x+face.x+w, y+face.y+h), (0, 0, 255), 1)
+            matrix = gray[y+face.y:face.y+y+h, face.x+x:face.x+x+w]
 
-draw_lines_again(img, face_list)
+            top_matrix = matrix[0:y, :]
+            bottom_matrix = matrix[y:y+h, :]
 
-# Display the output
-cv2.imshow('Ouput', img)
-cv2.waitKey()
+            #cv2.imshow(str(y), top_matrix)
+            #cv2.imshow(str(x), bottom_matrix)
+
+            color_ratio = compareColorAverage(top_matrix, bottom_matrix)
+            if color_ratio <= 20:
+                face.isMask = True
+            else:
+                face.isMask = False
+
+    # Draw things
+    for f in face_list:
+        print("MASK: ", f.isMask)
+        box_color = (0, 255, 0)
+        if f.isMask:
+            box_color = (0, 0, 255)
+        cv2.rectangle(img, (f.x, f.y), (f.x_end, f.y_end), box_color, 2)
+        # Display distance from camera
+        # cv2.putText(img,
+        #             str(f.est_dist),
+        #             (f.x, f.y),
+        #             f.font, 1,
+        #             (0, 255, 0),
+        #             2,
+        #             cv2.LINE_4)
+
+    draw_lines_again(img, face_list)
+
+    # Display the output
+    cv2.imwrite("./pictures/output.jpg", img)
+
+    output = cv2.imread("./pictures/output.jpg")
+    cv2.imshow("OUTPUT", output)
+
+    cv2.waitKey()
+
+
+root = Tk()
+
+frm = Frame(root)
+frm.pack(side=BOTTOM, padx=15, pady=15)
+
+lbl = Label(root)
+lbl.pack()
+
+btn = Button(frm, text="Upload Image", command=lambda: detect(1))
+btn.pack(side=tk.LEFT)
+
+btn3 = Button(frm, text="Upload from webcam", command=lambda: detect(0))
+btn3.pack(side=tk.LEFT, padx=10)
+
+btn2 = Button(frm, text="Exit", command=lambda: exit())
+btn2.pack(side=tk.LEFT, padx=10)
+
+root.title("Image Browser")
+root.geometry("300x350")
+root.mainloop()
